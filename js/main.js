c6a9819146c83a3bd069f3c3d56ed3e452f5b728
yu <1905784504@qq.com>
2014-04-10 23:13:43 +0800

    自定义二级级联select已完成

--- js/main.js
+++ js/main.js
@@ -8,4 +8,20 @@ $(function() {
 	$('#btn-pop').click(function() {
 		yu.popAlert('自定义alert的内容;更多更多的内容，，观察换行效果');
 	});
+
+	/* 初始化二级级联select，这时两级都没有值 */
+	yu.selectUtil.initArea('');
+
+	/* 当二级级联select的，一级有选中值时，动态生成二级 */
+	$('#province').change(function() {
+		var provinceVal = $(this).val();
+		yu.selectUtil.changeProvince('', provinceVal);
+	});
+
+	/* 回显用户之前用户选择过的二级级联select，要查看后果请把以下删除以下语句注释 */
+	yu.selectUtil.initArea('_1', '广东', '清远'); 
+	$('#province_1').change(function() {
+		var provinceVal = $(this).val();
+		yu.selectUtil.changeProvince('_1', provinceVal);
+	});	
 });
